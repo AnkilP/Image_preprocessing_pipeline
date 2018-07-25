4e0a310b7a719f5ae27fd89c729d143f29c1818b
AnkilP <a293pate@uwaterloo.ca>
2018-07-25 17:43:12 -0400

    edge detector for mlaa anti-aliasing

--- testing_environment.cpp
+++ testing_environment.cpp
@@ -9,15 +9,15 @@
 
 int main(int argc, char **argv) {
     cv::Mat street_image = cv::imread("street.jpg");
-    cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE); // Create a window for display.
     if (street_image.empty())                      // Check for invalid input
     {
         std::cout << "Could not open or find the image" << std::endl;
         return -1;
     }
-    cv::imshow("Display window", street_image);                // Show our image inside it.
-    cv::waitKey(5000); // Wait for a keystroke in the window
-    ImageConverter imageConverter();
+    //cv::imshow("Display window", street_image);                // Show our image inside it.
+    ImageConverter imageConverter(street_image);
+    imageConverter.edge_finder();
+    imageConverter.display_image();
     //first let's convert the image to Lab or YCrCb format
     return 0;
 }
